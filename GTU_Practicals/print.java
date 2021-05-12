ea20dcb30957e4488be9aec8ed97ce87e8c68170
BhavikSojitra <75220689+BhavikSojitra@users.noreply.github.com>
2021-05-12 19:24:52 +0530

    Create print.java

---
+++ GTU_Practicals/print.java
@@ -0,0 +1,9 @@
+/* Write a Program that displays Welcome to Java, Learning Java Now and Programming is fun. */
+
+package OOPJ;
+public class Print {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java,"); 	
+		System.out.println("Learning Java Now and Programming is fun."); 
+	}
+}
