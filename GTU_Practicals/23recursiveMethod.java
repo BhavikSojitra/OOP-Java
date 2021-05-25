91c13b31f29c2d0946f562301ee058a5fd32d46a
Bhavik Sojitra <75220689+BhavikSojitra@users.noreply.github.com>
2021-05-25 21:57:07 +0530

    Create 23recursiveMethod.java

---
+++ GTU_Practicals/23recursiveMethod.java
@@ -0,0 +1,47 @@
+/* Write a recursive method that returns the smallest integer in an array. 
+      Write a test program that prompts the user to enter an integer and display its product. */
+
+package OOPJ;
+import java.util.Scanner;
+
+public class recursiveMethod
+{
+ public static void main(String[] args) 
+ {
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("Enter five integers: ");
+  int[] list = new int[5]; 
+  for (int i = 0; i < list.length; i++) 
+  {
+   list[i] = input.nextInt();
+  }
+
+  System.out.println("The smallest element is " + min(list));
+ }
+
+
+ public static int min(int[] list) 
+ {
+  int min = list[list.length - 1];
+  int index = list.length - 1;
+  return min(list, index, min);
+ }
+
+ private static int min(int[] list, int index, int min) 
+ {
+  if (index < 0)
+  {
+   return min;
+  }
+  else if (list[index] < min) 
+  { 
+   return min(list, index - 1, list[index]);
+  }
+  else
+  {
+   return min(list, index - 1, min);
+  }
+ }
+}   
+
